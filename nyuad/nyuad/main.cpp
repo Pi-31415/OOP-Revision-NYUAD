91bde15063793280297ba93ef803fdb2a15b0a80
Pi-31415 <paingthetko.pi@gmail.com>
2020-09-07 18:16:13 +0630

    .

--- nyuad/nyuad/main.cpp
+++ nyuad/nyuad/main.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main(){
     int i;
+    int j;
+    cout<<"Please enter a number: ";
     cin>>i;
+    
+    cout<<"Please enter another number: ";
+    cin>>j;
+    
+    cout<<"You entered : " << i << " and " << j << endl;
+    return 0;
 }
