df18d5d2ca627ea2eec4eddaeac84a054545469f
Pi-31415 <paingthetko.pi@gmail.com>
2020-09-11 22:50:26 +0630

    .

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -5,6 +5,12 @@ print("1 = Rock")
 print("2 = Paper")
 print("3 = Scissor")
 # https://www.w3schools.com/python/python_user_input.asp
+
 choice = raw_input("Enter username:")
-print("Username is: " + username)
+
+try:
+    print(int(choice))
+except:
+    choice = raw_input("Please Enter a number :")
+
 
