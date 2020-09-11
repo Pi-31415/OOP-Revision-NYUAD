396a023153b48b96695d1c96824ece038f33bf9d
Pi-31415 <paingthetko.pi@gmail.com>
2020-09-11 23:37:14 +0630

    .

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -10,19 +10,19 @@ user_score = 0
 
 while(1 == 1):
     # User interface
-    print("\n\nRound "+str(game_round))
+    print("\n\nRound "+str(game_round)+"\n")
     print("1 = Rock\t\t\tScore")
-    print("2 = Scissor\t\t\tUser:"+str(user_score))
-    print("3 = Paper\t\t\tComputer:" + str(computer_score))
+    print("2 = Scissor\t\t\tUser : "+str(user_score))
+    print("3 = Paper\t\t\tComputer : " + str(computer_score)+"\n")
     # Get input from user, only accept integer with python error handling
     try:
-        user_choice = int(input("Choose a number:"))-1
+        user_choice = int(input("Choose a number : "))-1
         if(user_choice > 2):
-            user_choice = int(input("Please choose from 1,2 and 3 only :"))-1
+            user_choice = int(input("Please choose from 1,2 and 3 only : "))-1
         elif(user_choice < 0):
-            user_choice = int(input("Please choose from 1,2 and 3 only :"))-1
+            user_choice = int(input("Please choose from 1,2 and 3 only : "))-1
     except:
-        user_choice = input("Please enter number only :")
+        user_choice = int(input("Please enter number only : "))-1
 
     # make computer choose Rock, paper or scissor randomly, by making it select a random integer between 0 and 2
     computer_choice = random.randint(0, 2)
@@ -44,3 +44,5 @@ while(1 == 1):
     elif(computer_choice - user_choice == 2):
         print("You Lose")
         computer_score += 1
+
+    game_round += 1
