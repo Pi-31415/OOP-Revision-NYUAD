f8db40ea73923bf0220706fb01a2ccc67f37736b
Pi-31415 <paingthetko.pi@gmail.com>
2020-09-11 22:45:55 +0630

    .

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -1,6 +1,9 @@
 
 
 print("Rock,Paper, Scissor game")
+print("1 = Rock")
+print("2 = Paper")
+print("3 = Scissor")
 # https://www.w3schools.com/python/python_user_input.asp
 username = raw_input("Enter username:")
 print("Username is: " + username)
