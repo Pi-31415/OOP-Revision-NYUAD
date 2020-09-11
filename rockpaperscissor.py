80518f8d3407b185a1291e80683076924f9d5f6f
Pi-31415 <paingthetko.pi@gmail.com>
2020-09-11 23:08:19 +0630

    .

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -4,10 +4,10 @@ import random
 # User interface
 
 print("Rock,Paper, Scissor game")
-choices = ["Rock","Paper","Scissor"]
+choices = ["Rock","Scissor","Paper"]
 print("1 = Rock")
-print("2 = Paper")
-print("3 = Scissor")
+print("2 = Scissor")
+print("3 = Paper")
 # https://www.w3schools.com/python/python_user_input.asp
 
 # Get input from user, only accept integer with python error handling
