94a3d6656266f108bf904e1d573140cf2f1670a5
Pi-31415 <paingthetko.pi@gmail.com>
2020-09-11 23:10:38 +0630

    ,

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -2,22 +2,22 @@
 
 import random
 # User interface
+while(true):
+    print("Rock,Paper, Scissor game")
+    choices = ["Rock","Scissor","Paper"]
+    print("1 = Rock")
+    print("2 = Scissor")
+    print("3 = Paper")
+    # https://www.w3schools.com/python/python_user_input.asp
 
-print("Rock,Paper, Scissor game")
-choices = ["Rock","Scissor","Paper"]
-print("1 = Rock")
-print("2 = Scissor")
-print("3 = Paper")
-# https://www.w3schools.com/python/python_user_input.asp
+    # Get input from user, only accept integer with python error handling
 
-# Get input from user, only accept integer with python error handling
+    try:
+        user_choice = int(input("Enter username:"))-1
+    except:
+        user_choice = input("Please Enter a number :")
 
-try:
-    user_choice = int(input("Enter username:"))-1
-except:
-    user_choice = input("Please Enter a number :")
+    # make computer choose Rock, paper or scissor randomly
+    computer_choice = random.randrange(2)
 
-# make computer choose Rock, paper or scissor randomly
-computer_choice = random.randrange(2)
-
-print(computer_choice - user_choice)
+    print(computer_choice - user_choice)
