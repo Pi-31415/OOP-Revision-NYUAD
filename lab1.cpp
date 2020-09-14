8767da1e56a5ea0ed288db6a71563349b88d00c3
Pi-31415 <paingthetko.pi@gmail.com>
2020-09-14 18:28:55 +0630

    compiler

--- lab1.cpp
+++ lab1.cpp
@@ -1,22 +1,33 @@
-/*****************************************/
-/* My Name, My Student Number */
-/* Date. */
-/* This program prints out some messages: */
-/* on the screen. */
-/*****************************************/
-#include <iostream>      // Required for cout, cin, endl
-    using namespace std; // The compiler will use all the library filenames in std
+ #include <iostream>		// Required for cout, cin, endl
+ 
+using namespace std;	// The compiler will use all the library filenames in std
+ 
 // The main function - the starting point of our program
-int main()
-{
-    cout << ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!") << endl;
-    cout << ("This program used to have lots of problems, ") << endl;
-    cout << ("but if it prints this, you fixed them all.") << endl;
-    cout << (" *** Hurray! ***") << endl;
-    cout << ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!") << endl;
-    // Wait to press return
-    cin.get();
-    // Exit program
-    return 0;
-
+int main() {
+ 
+	// Declare and initialize objects
+	char myChar{'c'};
+	int myInt(3);
+	float myFloat = 5.4321f;
+ 
+	// Print values
+	cout << "Value of myChar: " << myChar << ", value of myInt: " << myInt << 
+	", value of myFloat: " << myFloat << endl;
+	
+	// Assign character to integer (Check Table 2.3 in the Textbook).
+	myInt = myChar;
+ 
+	// Print values
+	cout << "Value of myChar: " << myChar << ", value of myInt: " << myInt << 
+	", value of myFloat: " << myFloat << endl;
+ 
+	// Assign float to integer
+	myInt = myFloat;
+ 
+	// Print values, what do you notice?
+	cout << "Value of myChar: " << myChar << ", value of myInt: " << myInt << 
+	", value of myFloat: " << myFloat << endl;
+ 
+	// Exit program
+	return 0;
 }
