70a9632268275aa7e99ee9ac5304e58848f5d923
Pi-31415 <paingthetko.pi@gmail.com>
2020-09-14 18:31:23 +0630

    lab1 a

--- lab1.cpp
+++ lab1.cpp
@@ -1,33 +1,30 @@
- #include <iostream>		// Required for cout, cin, endl
- 
+#include <iostream>		// Required for cout, cin, endl
 using namespace std;	// The compiler will use all the library filenames in std
- 
 // The main function - the starting point of our program
 int main() {
- 
 	// Declare and initialize objects
-	char myChar{'c'};
+	char myChar('c');
 	int myInt(3);
 	float myFloat = 5.4321f;
- 
+
 	// Print values
 	cout << "Value of myChar: " << myChar << ", value of myInt: " << myInt << 
 	", value of myFloat: " << myFloat << endl;
-	
+
 	// Assign character to integer (Check Table 2.3 in the Textbook).
 	myInt = myChar;
- 
+
 	// Print values
 	cout << "Value of myChar: " << myChar << ", value of myInt: " << myInt << 
 	", value of myFloat: " << myFloat << endl;
- 
+
 	// Assign float to integer
 	myInt = myFloat;
- 
+
 	// Print values, what do you notice?
 	cout << "Value of myChar: " << myChar << ", value of myInt: " << myInt << 
 	", value of myFloat: " << myFloat << endl;
- 
+
 	// Exit program
 	return 0;
 }
