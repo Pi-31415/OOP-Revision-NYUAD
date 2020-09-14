efe5c01dc7fba6db6775642e9e10f2e8f146a745
Pi-31415 <paingthetko.pi@gmail.com>
2020-09-14 18:05:30 +0630

    .

--- lab1.cpp
+++ lab1.cpp
@@ -1,13 +1,13 @@
+#define K 5
+#define J 6
 #include <iostream>
 #include <iomanip>
 #include <ios>
-#define K 5;
 using namespace std;
 
 int main(){
     int i(2),j(4);
 
-    const int k = K;
-    
-    cout <<"Value of is "<< k <<endl;
+ 
+    cout <<"Value of is "<< (i << K) << endl;
 }
