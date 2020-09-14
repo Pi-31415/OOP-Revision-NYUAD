b7d35f08c479445e4acdc86ebfee8db1bd3b83b6
Pi-31415 <paingthetko.pi@gmail.com>
2020-09-14 18:21:39 +0630

    fixed

---
+++ linear_interpolation.cpp
@@ -0,0 +1,21 @@
+.
+/*****************************************/
+/* My Name, My Student Number */
+/* Date. */
+/* This program prints out some messages: */
+/* on the screen. */
+/*****************************************/
+#include <iostream>      // Required for cout, cin, endl
+    using namespace std; // The compiler will use all the library filenames in std
+// The main function - the starting point of our program
+int main()
+{
+    cout << ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!") << endl;
+    cout << ("This program used to have lots of problems, ") << endl;
+    cout << ("but if it prints this, you fixed them all.") << endl;
+    cout << (" *** Hurray! ***") << endl;
+    cout << ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!") << endl;
+    // Wait to press return
+    cin.get();
+    // Exit program
+    return 0;
