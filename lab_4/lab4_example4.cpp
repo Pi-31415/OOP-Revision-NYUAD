7721552d2638f9053d7911047c47956bb0c621c3
Pi-31415 <paingthetko.pi@gmail.com>
2020-10-05 18:51:03 +0630

    Example 4 done

--- lab_4/lab4_example4.cpp
+++ lab_4/lab4_example4.cpp
@@ -6,30 +6,51 @@ using namespace std;
 int main()
 {
     //This program copy the contents of one file (source) into another file (target)
-    string filename_source("");
-    string filename_target("");
+    // Create a text string, which is used to output the text file
+
+    string source_filename("");
+    string target_filename("");
 
     cout << "Enter source filename : ";
-    //Prompts user for the source filename in current directory
-    getline(cin, filename_source);
-
-    ifstream inputfile;
-    // uses io:: in opening mode only for reading
-    inputfile.open(filename_source, ios::in);
-
-    //Attempt to create a file if it does not exist
+    //Prompts user for the filename in current directory
+    getline(cin, source_filename);
+    ifstream inputfile(source_filename);
+    //Fall back if file cannot be open
     if (inputfile.fail())
     {
-        cerr << "File " << filename << " cannot be opened. Exiting program." << endl;
+        cout << "Cannot open file " << source_filename << "Exiting " << endl;
         exit(1);
     }
+
+    cout << "Enter target filename : ";
+    //Prompts user for the filename in current directory
+    getline(cin, target_filename);
+    ofstream outputfile(target_filename, ios::trunc);
+    //Fall back if file cannot be open
+    if (outputfile.fail())
+    {
+        cout << "Cannot open file " << target_filename << "Exiting " << endl;
+        exit(2);
+    }
+
+    //Then copy char by char
+    char ch;
+    while (!inputfile.eof())
+    {
+        inputfile.get(ch);
+        outputfile << ch;
+    }
+
+    cout << "Copying content from " << source_filename << " to " << target_filename << " successful." << endl;
+
+    inputfile.close();
+    outputfile.close();
 }
 
 /*
 Program output
 
-Enter filename to check : testfile.dat
-File testfile.dat does not exist in current directory. Would you like to create the file? (Y,n)
-Enter either Y or n :Y
-File with name testfile.dat is successfully created.
+Enter source filename : rocket1.txt
+Enter target filename : testfile.dat
+Copying content from rocket1.txt to testfile.dat successful.
 */
