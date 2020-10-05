406e41c49243a38242a1e83f9501522774b2488e
Pi-31415 <paingthetko.pi@gmail.com>
2020-10-05 19:47:36 +0630

    .

--- lab_4/lab4_example5.cpp
+++ lab_4/lab4_example5.cpp
@@ -19,16 +19,16 @@ int main()
     double time(0), altitude(0), velocity(0), acceleration(0);
     double maximum(0);
     double maximum_time(0);
-    
+
     for (int i = 0; i < numEntries; i++)
     {
         // Reading values
         inputfile >> time >> altitude >> velocity >> acceleration;
-        if(altitude > maximum){
+        if (altitude > maximum)
+        {
             maximum_time = time;
         }
-        
     }
-    cout << "Time for maximum altitude occurs at " <<maximum_time<<"s."<<endl;
+    cout << "Time for maximum altitude occurs at " << maximum_time << "s." << endl;
     inputfile.close();
 }
