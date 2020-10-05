f28736dff51aa206620c27922ccf879f7f5d4b90
Pi-31415 <paingthetko.pi@gmail.com>
2020-10-05 17:31:41 +0630

    Test Example 1

--- lab_4/lab4_example1.cpp
+++ lab_4/lab4_example1.cpp
@@ -3,44 +3,26 @@
 using namespace std;
  
 int main () {
-   char data[100];
+   int data(0);
 
-   // open a file in write mode.
    ofstream outfile;
    outfile.open("Number.txt");
 
-   cout << "Writing to the file" << endl;
-   cout << "Enter your name: "; 
-   cin.getline(data, 100);
-
-   // write inputted data into the file.
-   outfile << data << endl;
-
-   cout << "Enter your age: "; 
+   cout << "Enter 10 numbers." << endl;
+   cout << "Number 1 : "; 
    cin >> data;
-   cin.ignore();
-   
-   // again write inputted data into the file.
+
    outfile << data << endl;
 
-   // close the opened file.
    outfile.close();
 
-   // open a file in read mode.
    ifstream infile; 
-   infile.open("afile.dat"); 
- 
+   infile.open("Number.txt"); 
+
    cout << "Reading from the file" << endl; 
    infile >> data; 
-
-   // write the data at the screen.
    cout << data << endl;
    
-   // again read the data from the file and display it.
-   infile >> data; 
-   cout << data << endl; 
-
-   // close the opened file.
    infile.close();
 
    return 0;
