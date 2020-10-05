1b8b626bc62b8f1a4dd8cc08cc549a624e700729
Pi-31415 <paingthetko.pi@gmail.com>
2020-10-05 17:56:36 +0630

    Example2 complete

--- lab_4/lab4_example2.cpp
+++ lab_4/lab4_example2.cpp
@@ -12,6 +12,7 @@ int main()
     getline(cin, filename);
 
     ifstream inputfile;
+    // uses io:: in opening mode only for reading
     inputfile.open(filename, ios::in);
 
     if (inputfile.fail())
@@ -21,23 +22,46 @@ int main()
     }
 
     //initialize variables for counting
-    int words(0), lines(0);
+    int words(1), lines(1);
     //character to read the characters
     char ch;
+    cout <<endl << "File Contents : " <<endl<<endl;
     while (!inputfile.eof())
     {
         inputfile.get(ch);
         if(ch == '\n'){
             //count as line when there is a new line character
+            // Initial value of line is 1 since 1 new line character makes 2 lines
             lines++;
         }
         if(ch == '\n' || ch == '\t' || ch == 0x20 || ch == 0x0d){
             //count as word when there is a new line, tab or space or carriage return
+            // Initial value of line is 1 since 1 white space character makes 2 words
             words++;
         }
+        //Print the file for visual analysis
         cout << ch;
     }
-
+    cout <<endl <<endl << "File Information" <<endl;
     cout << "Number of lines : " <<lines<<endl;
+    cout << "Number of words : " <<words<<endl;
 
 }
+
+/*
+Program Output
+----------------------------------------------
+
+Enter filename to analyze : example2text.txt
+
+File Contents : 
+
+Lorem ipsum dolor sit amet, consectetur
+adipiscing elit, sed do eiusmod tempor incididunt..
+
+File Information
+Number of lines : 2
+Number of words : 13
+(base) Paings-MacBook-Pro-2:lab_4 paingthetko$ 
+
+*/
