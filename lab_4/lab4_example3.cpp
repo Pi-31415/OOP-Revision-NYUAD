918f149cf0551efaaabc20d29ce13356d1061db7
Pi-31415 <paingthetko.pi@gmail.com>
2020-10-05 18:22:47 +0630

    Example 3

--- lab_4/lab4_example3.cpp
+++ lab_4/lab4_example3.cpp
@@ -5,12 +5,13 @@ using namespace std;
 
 int main()
 {
-    // This program checks if a file exists or not. 
-    //If the file does not exist, then the program prompts the user whether she/he wants to create it or not. 
+    // This program checks if a file exists or not.
+    //If the file does not exist, then the program prompts the user whether she/he wants to create it or not.
 
     string filename("");
 
     cout << "Enter filename to check : ";
+    //Prompts user for the filename in current directory
     getline(cin, filename);
 
     ifstream inputfile;
@@ -23,13 +24,30 @@ int main()
         cerr << "File " << filename << " does not exist in current directory. Would you like to create the file? (Y,n)" << endl;
         //If file fails to open, then prompt the user a choice to create the file.
         string user_choice("n");
+        cout << "Enter either Y or n :";
         getline(cin, user_choice);
-        if(user_choice == "Y"){
-
+        if (user_choice == "Y")
+        {
+            // Create the file if user says Y
+            ofstream outfile;
+            outfile.open(filename);
+            if (!outfile.fail())
+            {
+                cerr << "File with name "<<filename<<" is successfully created."<<endl;
+            }
         }
-        
-    }else{
+    }
+    else
+    {
         cout << "File " << filename << " exists." << endl;
     }
-
 }
+
+/*
+Program output
+
+Enter filename to check : testfile.dat
+File testfile.dat does not exist in current directory. Would you like to create the file? (Y,n)
+Enter either Y or n :Y
+File with name testfile.dat is successfully created.
+*/
