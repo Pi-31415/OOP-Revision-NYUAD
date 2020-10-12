e1be27e11ba4f845e87650c51be66edefdac3101
Pi-31415 <paingthetko.pi@gmail.com>
2020-10-12 18:49:17 +0630

    .

--- lab_5/example2.cpp
+++ lab_5/example2.cpp
@@ -32,13 +32,23 @@ int check_prime(int number)
 
 int main()
 {
+    ofstream file;
+    file.open ("Prime.txt");
+    
     cout << "Prime Checking Program"<<endl;
     for (int i = 2; i <= 100; i++)
     {
         if(check_prime(i) != 0){
             cout << check_prime(i) << endl;
+            file << check_prime(i) << endl;
         }
     }
+    file.close();
 
     return 0;
 }
+
+/*
+Write a function that determines whether a number is prime or not (the function should return true if the number is prime and false otherwise). Use this function to find all prime numbers between 2 and 10000 and store them in a text file “Prime.txt”. Read the data back from “Prime.txt” and display it on the standard output. 
+
+*/
