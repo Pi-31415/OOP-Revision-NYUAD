ffc6bf92024b1da31b4cc8109345e589c6b94375
Pi-31415 <paingthetko.pi@gmail.com>
2020-10-12 19:15:39 +0630

    Push

--- lab_5/example3.cpp
+++ lab_5/example3.cpp
@@ -3,19 +3,40 @@ using namespace std;
 
 double Cel_to_Fahr(double celsius)
 {
-    return (celsius * (9 / 5)) + 32;
+    return (celsius*1.8) + 32;
 }
 
 double Cel_to_Kel(double celsius)
 {
     return celsius + 273.2;
 }
-
+// This program takes the input as Celcius and gives user the choice to calculate Kelvin or Fahrenheit
 int main()
 {
+    int user_input(0),user_choice(0);
+    //Put user in an infinite loop until eof character is detected
     while (!cin.eof())
     {
-        cout << Cel_to_Kel(100);
+        cout << endl << "Temperature converter, select an option either 1 or 2." << endl;
+        cout << "1. Celsius to Fahrenheit"<<endl;
+        cout << "2. Celsius to Kelvin"<<endl;
+        cout << "Hit Ctrl + D to exit."<<endl;
+        //Prompt user for choice
+        cout << endl <<"Choice : ";
+        cin >> user_choice;
+        if (user_choice == 1){
+            cout << "Please enter a temperature in Celsius : ";
+            cin >> user_input;
+            cout <<user_input <<" Celsius = "<< Cel_to_Fahr(user_input) << " Fahrenheit" << endl;
+        }
+        else if(user_choice == 2){
+            cout << "Please enter a temperature in Celsius : ";
+            cin >> user_input;
+            cout <<user_input <<" Celsius = " << Cel_to_Kel(user_input) << " Kelvin" << endl;
+        }else{
+            cout << "Please enter a valid choice, or Ctrl + D to exit." << endl;
+        }
+        
     }
     return 0;
 }
