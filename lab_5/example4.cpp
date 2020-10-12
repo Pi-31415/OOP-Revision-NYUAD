ef6615ca046c08f1d36457edc7d04b45b2bee88e
Pi-31415 <paingthetko.pi@gmail.com>
2020-10-12 19:29:47 +0630

    Example 4 done

---
+++ lab_5/example4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int swap_variables(int a, int b, int &x, int &y)
+{
+    a = x;
+    b = y;
+    x = b;
+    y = a;
+}
+
+int main()
+{
+    int x(1),y(2);
+    cout << "Before Swapping" <<endl;
+    cout << "x : " <<x <<endl;
+    cout << "y : " <<y <<endl;
+    //swap the variables
+    swap_variables(x,y,x,y);
+    cout << "After Swapping" <<endl;
+    cout << "x : " <<x <<endl;
+    cout << "y : " <<y <<endl;
+    return 0;
+}
