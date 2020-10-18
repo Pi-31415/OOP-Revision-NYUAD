2560ee31fc270d9ba7b5da5cf6f3d727c2cf5f3c
Pi-31415 <paingthetko.pi@gmail.com>
2020-10-18 18:44:30 +0630

    done exercise 5

--- lab_6/exercise5.cpp
+++ lab_6/exercise5.cpp
@@ -5,20 +5,22 @@
 #define MAX_QUESTIONS 100
 using namespace std;
 
-void get_answers(char answer[]);
+void get_answers(char answer[], int &max_index);
+void print_answers(const char answer[], int max_index);
 
 int main()
 {
     char answers[MAX_QUESTIONS];
-
+    int max_index(0); // this is the number of actual answers in file
     //sensor1 >> x; // read data from file and save in x
 
-    get_answers(answers);
+    get_answers(answers, max_index);
+    print_answers(answers, max_index);
 
     return 0;
 }
 
-void get_answers(char answer[])
+void get_answers(char answer[], int &max_index)
 {
     char ch; //character to represent each char in file read
     ifstream file;
@@ -31,9 +33,32 @@ void get_answers(char answer[])
     //assume that there is space after the answers,
     //and first line contains the answers
     // i.e. 0000 ABCDABCDABCD
-    while (!file.eof() || ch != '\n')
+    while (!file.eof())
     {
         file.get(ch);
-        cout << ch;
+        if (ch == '\n')
+        {
+            //break the reading at the end of first line
+            break;
+        }
+        if (ch != '0' && ch != ' ')
+        {
+            //if the character is not 0 and not space, it is the answer
+            answer[max_index] = ch;
+            max_index++;
+        }
     }
+}
+
+void print_answers(const char answer[], int max_index)
+{
+    //this prints the correct answers
+    cout << "Correct Answers : ";
+    for  (int i=0;i< max_index;i++){
+        cout << answer[i];
+        if(i != max_index-1){
+            cout << ",";
+        }
+    }
+    cout <<endl;
 }
