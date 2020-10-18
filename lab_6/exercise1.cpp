32e867a6e8a8332135fbc0be374c534d363d194b
Pi-31415 <paingthetko.pi@gmail.com>
2020-10-18 17:47:05 +0630

    Exercise 2 Done

--- lab_6/exercise1.cpp
+++ lab_6/exercise1.cpp
@@ -29,3 +29,20 @@ int main()
 
     return 0;
 }
+
+/*
+Output
+
+Array Index    Array Content  
+0              0              
+1              1              
+2              2              
+3              3              
+4              4              
+5              5              
+6              6              
+7              7              
+8              8              
+9              9   
+
+*/
---
+++ lab_6/exercise2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int detect_missing(const int arr[],int n);
+
+int main(){
+    //missing number is 3
+    int integers[4] = {1,2,4,5};
+
+    cout << "Missing integer is " << detect_missing(integers,5) << endl;
+
+    return 0;
+}
+
+int detect_missing(const int arr[],int n){
+
+    int total = 0;
+
+    for(int i=0;i<(n-1);i++){
+        //find the total of all integers in array
+        total += arr[i];
+    }
+
+    //return the missing integer using the formula
+    int missing_integer = (((n)*(n+1))/2) - total ;
+
+    return missing_integer;
+}
+
+/*
+Output
+
+Missing integer is 3
+
+*/
