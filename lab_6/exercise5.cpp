14326980de72cd274813d5049b0572cca12774ed
Pi-31415 <paingthetko.pi@gmail.com>
2020-10-18 18:34:58 +0630

    .

--- lab_6/exercise5.cpp
+++ lab_6/exercise5.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
 #include <fstream>
+
+#define FILE_NAME "answers.txt"
+#define MAX_QUESTIONS 100
 using namespace std;
 
-void readData();
+void get_answers(char answer[]);
 
 int main()
 {
+    char answers[MAX_QUESTIONS];
 
     //sensor1 >> x; // read data from file and save in x
+
+    get_answers(answers);
 
     return 0;
 }
 
-void readData()
+void get_answers(char answer[])
 {
+    char ch; //character to represent each char in file read
     ifstream file;
-    file.open("");
+    file.open(FILE_NAME);
     if (file.fail()) //detect file open failure
     {
         cerr << "Answer file could not be opened.";
         exit(1);
+    }
+    while (!file.eof())
+    {
+        infile.get(ch);
+        cout << ch;
     }
 }
