626171b9e7d0e1982c4de4bc0c4dc45d6ce3e717
Pi-31415 <paingthetko.pi@gmail.com>
2020-10-18 18:01:26 +0630

    Exercise 3 done

--- lab_6/exercise3.cpp
+++ lab_6/exercise3.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-
-#include <iostream>
+#include <iomanip>
+#define COLUMN_SIZE 10
 
 using namespace std;
 
@@ -8,7 +8,20 @@ void plot (int n);
 
 int main(){
     int value[10] = {19,3,15,7,11,9,13,5,17,1};
-    plot(5);
+    int counter(0);
+
+    //print headers
+    cout.setf(ios::left);
+    cout <<setw(COLUMN_SIZE)<< "Element" <<setw(COLUMN_SIZE)<< "Value" << setw(COLUMN_SIZE)<<"Histogram"<<endl;
+
+    for(int i:value){
+        //print tabular data
+        cout <<setw(COLUMN_SIZE)<< counter <<setw(COLUMN_SIZE)<< i;
+        plot(i);
+        cout <<endl;
+        counter++;
+    }
+    //plot(5);
     return 0;
 }
 
@@ -18,3 +31,18 @@ void plot(int n){
         cout <<"*";
     }  
 }
+
+/*
+Output
+Element   Value     Histogram 
+0         19        *******************
+1         3         ***
+2         15        ***************
+3         7         *******
+4         11        ***********
+5         9         *********
+6         13        *************
+7         5         *****
+8         17        *****************
+9         1         *
+*/
