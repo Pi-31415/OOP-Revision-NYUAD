f78410b956fea13308e7f252f46d11da9406fbed
Pi-31415 <paingthetko.pi@gmail.com>
2020-10-18 17:33:19 +0630

    exercise 1 done

--- lab_6/exercise1.cpp
+++ lab_6/exercise1.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <iomanip>
 
+#define ARR_SIZE 10
+#define COLUMN_SIZE 15
 using namespace std;
 
-int main(){
+int main()
+{
+    //declare an array with the size of 10
+    int arr[ARR_SIZE];
+
+    for (int i = 0; i < ARR_SIZE; i++)
+    {
+        //assign the index of element in element
+        arr[i] = i;
+    }
+
+    //set header
+    cout.setf(ios::left);
+    cout << setw(COLUMN_SIZE) << "Array Index";
+    cout << setw(COLUMN_SIZE) << "Array Content" << endl;
+
+    //print out the content of array
+    for (int i = 0; i < ARR_SIZE; i++)
+    {
+        cout << setw(COLUMN_SIZE) << i << setw(COLUMN_SIZE) << arr[i] << endl;
+    }
 
     return 0;
 }
