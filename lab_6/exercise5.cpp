a79dae950aac1af43c2b840c0a932192582aed8e
Pi-31415 <paingthetko.pi@gmail.com>
2020-10-18 18:31:09 +0630

    .

--- lab_6/exercise5.cpp
+++ lab_6/exercise5.cpp
@@ -2,18 +2,23 @@
 #include <fstream>
 using namespace std;
 
+void readData();
+
 int main()
 {
-    ifstream sensor1;
-    sensor1.open("sensor1.txt");
-    if (sensor1.fail()) //detect file open failure
-    {
-        cerr << "Answer file could not be opened.";
-        exit(1); 
-    }
 
-    
     //sensor1 >> x; // read data from file and save in x
 
     return 0;
+}
+
+void readData()
+{
+    ifstream file;
+    file.open("");
+    if (file.fail()) //detect file open failure
+    {
+        cerr << "Answer file could not be opened.";
+        exit(1);
+    }
 }
