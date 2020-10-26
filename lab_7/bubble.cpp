76f7c34d367896608d5147367f61ccf50edd8fa4
Pi-31415 <paingthetko.pi@gmail.com>
2020-10-26 17:59:01 +0630

    bubble sort done

---
+++ lab_7/bubble.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+void swap(int &a, int &b) {
+   int temp;
+   temp = a;
+   a = b;
+   b = temp;
+}
+void print_array(int *array, int size) {
+   for(int i = 0; i<size; i++)
+      cout << array[i] << " ";
+   cout << endl;
+}
+void selectionSort(int *array, int size) {
+   int i, j, imin;
+   for(i = 0; i<size-1; i++) {
+      imin = i;  
+      for(j = i+1; j<size; j++)
+         if(array[j] < array[imin])
+            imin = j;
+         swap(array[i], array[imin]);
+   }
+}
+
+void selectionSortReverse(int *array, int size) {
+   int i, j, imin;
+   for(i = 0; i<size-1; i++) {
+      imin = i;   
+      for(j = i+1; j<size; j++)
+         if(array[j] > array[imin])
+            imin = j;
+         swap(array[imin], array[i]);
+   }
+}
+
+int main() {
+   int n;
+   cout << "Enter the number of elements: ";
+   cin >> n;
+   int arr[n];
+   
+   for(int i = 0; i<n; i++) {
+      cout << "Enter elements # "<<(i+1)<<" : ";
+      cin >> arr[i];
+   }
+   cout << endl;
+   cout << "Created Array : ";
+   print_array(arr, n);
+   selectionSort(arr, n);
+   cout << "Bubble Sort (Ascending) : ";
+   print_array(arr, n);
+   selectionSortReverse(arr, n);
+   cout << "Bubble Sort (Descending) : ";
+   print_array(arr, n);
+}
