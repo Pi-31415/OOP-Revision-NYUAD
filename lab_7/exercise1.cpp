a1219d59168c4fbeefd92e327381e14321cd68aa
Pi-31415 <paingthetko.pi@gmail.com>
2020-10-26 17:37:49 +0630

    .

--- lab_7/exercise1.cpp
+++ lab_7/exercise1.cpp
@@ -1,23 +1,39 @@
-// C++ Program to print the elements of a 
-// Two-Dimensional array 
-#include<iostream> 
-using namespace std; 
-
-int main() 
-{ 
-	// an array with 3 rows and 2 columns. 
-	int x[3][2] = {{0,1}, {2,3}, {4,5}}; 
-
-	// output each array element's value 
-	for (int i = 0; i < 3; i++) 
-	{ 
-		for (int j = 0; j < 2; j++) 
-		{ 
-			cout << "Element at x[" << i 
-				<< "][" << j << "]: "; 
-			cout << x[i][j]<<endl; 
-		} 
-	} 
-
-	return 0; 
-} 
+#include<iostream>
+using namespace std;
+void swapping(int &a, int &b) {         //swap the content of a and b
+   int temp;
+   temp = a;
+   a = b;
+   b = temp;
+}
+void display(int *array, int size) {
+   for(int i = 0; i<size; i++)
+      cout << array[i] << " ";
+   cout << endl;
+}
+void selectionSort(int *array, int size) {
+   int i, j, imin;
+   for(i = 0; i<size-1; i++) {
+      imin = i;   //get index of minimum data
+      for(j = i+1; j<size; j++)
+         if(array[j] < array[imin])
+            imin = j;
+         //placing in correct position
+         swap(array[i], array[imin]);
+   }
+}
+int main() {
+   int n;
+   cout << "Enter the number of elements: ";
+   cin >> n;
+   int arr[n];           //create an array with given number of elements
+   cout << "Enter elements:" << endl;
+   for(int i = 0; i<n; i++) {
+      cin >> arr[i];
+   }
+   cout << "Array before Sorting: ";
+   display(arr, n);
+   selectionSort(arr, n);
+   cout << "Array after Sorting: ";
+   display(arr, n);
+}
