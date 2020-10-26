655de556aa5ff65dcd020479b4da5142e23787d2
Pi-31415 <paingthetko.pi@gmail.com>
2020-10-26 22:09:03 +0630

    .

--- quizzes/5.cpp
+++ quizzes/5.cpp
@@ -1,25 +1,27 @@
-#include<iostream>
-#include<fstream>
 
-#define ROW_SIZE 2
-#define COL_SIZE 6
+#include<iostream>
 
 using namespace std;
 
+#define SIZE 10
+
 int main(){
-    char letters[ROW_SIZE][COL_SIZE] = {'d', 'e', 'a', 'r', ' ', ' ', 'f', 'r', 'i', 'e', 'n', 'd'};
+    int nums[SIZE] = {1, 3, 5, 2, 4, 9, 7, 10, 8, 6};
     
-    ifstream inFile("myFile.txt", ios::in);
-    if(inFile.fail()) exit(1);
+    for(int i = 0; i < SIZE-1; i++){
+        int special_index = i;
 
-    for(int i = 0; i < COL_SIZE; i++)
-        inFile.get(letters[1][i]);
+        for(int j = i+1; j < SIZE; j++){
+            if(nums[j] > nums[special_index])
+                special_index = j;
+        }
+        int temp= nums[i];
+        nums[i] = nums[special_index];
+        nums[special_index] = temp;
+    }
 
-    for(int i = 0; i < ROW_SIZE; i++)
-        for(int j = 0; j < COL_SIZE; j++)
-            cout << letters[i][j];
-    
-    inFile.close();
+    for(int i = 0; i < SIZE; i++)
+        cout << nums[i] << "";
+
     return 0;
 }
- 
