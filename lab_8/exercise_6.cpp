d1a284f99de3fdb1880214c97b93bd4d94b88114
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-02 19:13:48 +0630

    .

---
+++ lab_8/exercise_6.cpp
@@ -0,0 +1,10 @@
+/*
+Prompt:
+Write a program that dynamically resize an array to a bigger size. 
+The program creates an array of an arbitrary size (specified by the user). 
+Then the user may extend the array to a bigger size (this involves creating a temporary 
+array with larger size, copying the values of the elements, and adding new values into 
+the extension cells). 
+Print the new array to the user to confirm the functionality.
+
+*/
