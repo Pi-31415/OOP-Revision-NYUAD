e690348001e04ee1fa09c667c73be07c07601c92
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-02 18:32:14 +0630

    .

--- lab_8/exercise_4.cpp
+++ lab_8/exercise_4.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int char_count (char *ptr, char c);
 
+int main(){
+    int times(0);
+    char mystring[] = "Test String";
+    char_count(&mystring[0],'s');
     return 0;
+}
+
+int char_count (char *ptr, char c){
+
+    cout << *ptr << endl;
+    ptr++;
+    cout << *ptr << endl;
+    ptr++;
 }
