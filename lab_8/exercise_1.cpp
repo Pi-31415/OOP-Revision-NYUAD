a538ee7f110cc17c7b9eda3b4c7c893ea040b26d
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-02 17:45:20 +0630

    .

--- lab_8/exercise_1.cpp
+++ lab_8/exercise_1.cpp
@@ -11,20 +11,27 @@ int main()
     cout << x << endl;  //expected = 130
     cout << ptr << endl; //expected = 8688
     
+    /*
+    Variable x into memory at address 8688 and the ptr variable at 8684. 
+    */
     x = 131;
     cout << x << endl; //expected = 131
     cout << &ptr << endl; //expected = 8684
 
+    /*
+    Variable x into memory at address 8688 and the ptr variable at 8684. 
+    */
     x = 132;
-    cout << &x << endl; // expected = 
-    cout << ptr << endl; // expected = 
+    cout << &x << endl; // expected = 8688
+    cout << ptr << endl; // expected = 8688
 
     x = 133;
-    cout << &x << endl;// expected = 
-    cout << *ptr << endl;// expected = 
+    cout << &x << endl;// expected = 8688
+    cout << *ptr << endl;// expected = 8684
 
     if (x != *ptr)
     {
+
         cout
             << "Hello1" << endl;
     }
@@ -32,6 +39,7 @@ int main()
     {
         cout << "Hello2" << endl;
     }
+    // Expected to output hello2 *ptr directly points to the contents of variable x
 
     return 0;
 }
