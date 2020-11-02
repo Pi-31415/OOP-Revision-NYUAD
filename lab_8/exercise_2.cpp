10e1a15046945a34ac3bcdc76b11dab1f9bc654e
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-02 17:48:59 +0630

    .

---
+++ lab_8/exercise_2.cpp
@@ -0,0 +1,25 @@
+/*
+Original Code before correction
+
+void main()
+{
+ int x, y=131, *ptr;
+ x=132;
+ y=*ptr;
+ cout << x << endl;
+ cout << y << endl;
+ return 0;
+}
+*/
+
+#include <iostream> 
+using namespace std;
+
+void main()
+{
+ int x, y=131, *ptr;
+ x=132;
+ y=*ptr;
+ cout << x << endl;
+ cout << y << endl;
+}
