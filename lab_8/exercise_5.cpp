eb5ee62b574864543a6d93e6fae00f0969e94ab5
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-02 18:43:44 +0630

    .

--- lab_8/exercise_5.cpp
+++ lab_8/exercise_5.cpp
@@ -3,7 +3,15 @@
 
 using namespace std;
 
-int main(){
-    
+int main()
+{   int x(0),y(0),time(0);
+    ifstream robotdata;
+    robotdata.open("robotdata.dat");
+    if (robotdata.fail()) 
+    {
+        cerr << "File could not be opened";
+        exit(1); 
+    }
+    robotdata >> x; // read data from file and save in x
     return 0;
 }
