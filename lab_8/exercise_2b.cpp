4eeb1b5c8c2cc6cdeecda55aa63d342896787f71
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-02 17:59:53 +0630

    source

---
+++ lab_8/exercise_2b.cpp
@@ -0,0 +1,39 @@
+/*
+Code before 
+
+#include <iostream>
+ 
+void main()
+{
+ int x,y=131, *ptr;
+ x=132;
+ ptr=&x;
+ &y=ptr;
+ cout << x << endl;
+ cout << y << endl;
+ return 0;
+}
+
+
+
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y = 131, *ptr;
+    x = 132;
+    ptr = &x;
+    y = *ptr;
+    cout << x << endl;
+    cout << y << endl;
+    return 0;
+}
+
+
+/*
+Output
+132 (as x)
+132 (as y through pointer)
+*/
