15db2a2a45aabafb85fb67f3f569dae12bdcdfff
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-02 19:04:25 +0630

    .

--- lab_8/exercise_5.cpp
+++ lab_8/exercise_5.cpp
@@ -3,6 +3,19 @@
 
 using namespace std;
 
+/*
+Question:
+
+Write a program that reads a data file that contains the time (second) 
+and coordinates of the position of a robot that is roaming in unknown space. 
+The first row stores the number of readings. Each subsequent line includes the 
+time, the x coordinate, and the y coordinate for the robot. The program should 
+determine the times when the robot comes inside a circle centered at the origin 
+with a radius R (entered by the user). Use dynamic memory allocation to create 
+run-time arrays for storing the time, x coordinate, and y coordinate.
+
+*/
+
 int main()
 {
     double *xptr,*yptr,*tptr;
