94605766341cc0fc0708c78ebf4fc15551dd85b9
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-02 18:33:35 +0630

    .

--- lab_8/exercise_4.cpp
+++ lab_8/exercise_4.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int char_count (char *ptr, char c);
+int char_count(char *ptr, char c);
 
-int main(){
+int main()
+{
     int times(0);
     char mystring[] = "Test String";
-    char_count(&mystring[0],'s');
+    char_count(&mystring[0], 's');
     return 0;
 }
 
-int char_count (char *ptr, char c){
-
-    cout << *ptr << endl;
-    ptr++;
-    cout << *ptr << endl;
-    ptr++;
+int char_count(char *ptr, char c)
+{
+    int times(0);
+    while (*ptr != '\0')
+    {
+        if(*ptr == c){
+            times++;
+        }
+        cout << *ptr << endl;
+        ptr++;
+        
+    }
+    return times;
 }
