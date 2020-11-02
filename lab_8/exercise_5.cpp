8577cd82cbee37ce564cc448bd50526f603bd97a
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-02 18:47:52 +0630

    Update exercise_5.cpp

--- lab_8/exercise_5.cpp
+++ lab_8/exercise_5.cpp
@@ -4,14 +4,22 @@
 using namespace std;
 
 int main()
-{   int x(0),y(0),time(0);
+{
+    int x(0), y(0), time(0), radius(0);
     ifstream robotdata;
     robotdata.open("robotdata.dat");
-    if (robotdata.fail()) 
+    if (robotdata.fail())
     {
         cerr << "File could not be opened";
-        exit(1); 
+        exit(1);
     }
-    robotdata >> x; 
+
+    for (int i = 0; i < ; i++)
+    {
+        robotdata >>
+    }
+
+    robotdata.close();
+
     return 0;
 }
