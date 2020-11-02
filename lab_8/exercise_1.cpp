09d7e7fefffb86c16447434f70d79645f340e515
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-02 17:46:41 +0630

    .

--- lab_8/exercise_1.cpp
+++ lab_8/exercise_1.cpp
@@ -4,30 +4,36 @@ int main()
 {
 
     /*
+    Question:
+    Assuming that the statement int x, *ptr=&x; places the variable x into memory at 
+    address 8688 and the ptr variable at 8684.  Write down the output you expect for 
+    the following code
+    */
+    /*
     Variable x into memory at address 8688 and the ptr variable at 8684. 
     */
     int x = 130;
-    int *ptr=&x; 
-    cout << x << endl;  //expected = 130
+    int *ptr = &x;
+    cout << x << endl;   //expected = 130
     cout << ptr << endl; //expected = 8688
-    
+
     /*
     Variable x into memory at address 8688 and the ptr variable at 8684. 
     */
     x = 131;
-    cout << x << endl; //expected = 131
+    cout << x << endl;    //expected = 131
     cout << &ptr << endl; //expected = 8684
 
     /*
     Variable x into memory at address 8688 and the ptr variable at 8684. 
     */
     x = 132;
-    cout << &x << endl; // expected = 8688
+    cout << &x << endl;  // expected = 8688
     cout << ptr << endl; // expected = 8688
 
     x = 133;
-    cout << &x << endl;// expected = 8688
-    cout << *ptr << endl;// expected = 8684
+    cout << &x << endl;   // expected = 8688
+    cout << *ptr << endl; // expected = 133 (contents of x)
 
     if (x != *ptr)
     {
