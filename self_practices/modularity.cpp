4fd3eeff157b06f819855b6318eefab395ee074b
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-06 18:30:14 +0630

    .

--- self_practices/modularity.cpp
+++ self_practices/modularity.cpp
@@ -1,18 +1,31 @@
-#include <iostream>
+/*---------------------------------------------------------*/
+/* Program chapter7_1 */
+/* This program assigns a set of values to a */
+/* one-dimensional array then prints a list of the array */
+/* offsets and values to standard output. */
+/*---------------------------------------------------------*/
+#include <iostream> //Required for cout.
+#include <iomanip>  //Required for setw().
 using namespace std;
-void ch6_static();
 int main()
 {
-    ch6_static();
-    ch6_static();
-    ch6_static();
-}
-void ch6_static()
-{
-    int x(0);
-    static int count(0);
-    x++;
-    count++;
-    cout << x << endl;
-    return;
+    //Declare varialbles.
+    double t[21];
+    //The array.
+    int i;
+    //The loop index.
+    //Assign 21 value to array t.
+    for (i = 0; i < 21; ++i)
+    {
+        t[i] = i * 0.5; //i provides offset and value.
+    }
+    //Print list of array offsets and values.
+    //Print heading.
+    cout << "21 values assigned to t" << endl
+         << "Offset Value" << endl; //Print list inside for loop.
+    for (i = 0; i < 21; ++i)
+    {
+        cout << setw(6) << i << setw(10) << t[i] << endl;
+    }
+    return 0;
 }
