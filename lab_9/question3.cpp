43df110fff1fb1662017d75bd343e604742fbb8a
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-09 18:38:27 +0630

    .

--- lab_9/question3.cpp
+++ lab_9/question3.cpp
@@ -4,38 +4,53 @@ using namespace std;
 
 class Point
 {
-    private:
-        int x;
-        int y;
+private:
+    int x;
+    int y;
 
-    public:
-        Point(int X = 0, int Y = 0)
-        {
-            x = X;
-            y = Y;
-        }
-        int getX(){
-            return x;
-        }
-        int getY(){
-            return y;
-        }
-        void setX(int X){
-            x = X;
-        }
-        void setY(int Y){
-            y = Y;
-        }
-        void setXY(int X, int Y)
-        {
-            x = X;
-            y = Y;
-        }
-
-    double getMagnitude(){
-        return sqrt(pow(x,2)+pow(y,2));
+public:
+    Point(int X = 0, int Y = 0)
+    {
+        x = X;
+        y = Y;
+    }
+    int getX()
+    {
+        return x;
+    }
+    int getY()
+    {
+        return y;
+    }
+    void setX(int X)
+    {
+        x = X;
+    }
+    void setY(int Y)
+    {
+        y = Y;
+    }
+    void setXY(int X, int Y)
+    {
+        x = X;
+        y = Y;
     }
 
+    double getMagnitude()
+    {
+        return sqrt(pow(x, 2) + pow(y, 2));
+    }
+
+    double getArgument()
+    {
+        return atan2(y, x);
+    }
+
+    void print()
+    {
+        cout << "x : " << x << endl
+             << "y : " << y << endl;
+    }
 };
 
 int main()
