b19d789a194c1861b84fc1f8aa54032e6ab9d014
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-09 17:43:27 +0630

    .

---
+++ lab_9/question2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Time
+{
+private:
+    
+
+public:
+    //Constructor
+    
+
+    
+
+};
+
+int main()
+{
+
+
+    return 0;
+}
