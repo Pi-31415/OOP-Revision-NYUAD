1ad2f71b01ccbdaa004f62ef7520bd93d85bacfb
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-09 18:53:50 +0630

    .

--- lab_9/question4.cpp
+++ lab_9/question4.cpp
@@ -1,30 +1,42 @@
 #include <iostream>
 using namespace std;
 
-class Account{
-    public:
-        int AccountNumber;
-        double balance;
+class Account
+{
+public:
+    int AccountNumber;
+    double balance;
 
-    private:
-        Account(int an = 1234,double b = 0.0){
-            AccountNumber = an;
-            balance = b;
-        }
+private:
+    Account(int an = 1234, double b = 0.0)
+    {
+        AccountNumber = an;
+        balance = b;
+    }
 
-        int getAccountNumber(){
-            return AccountNumber;
-        }
+    int getAccountNumber()
+    {
+        return AccountNumber;
+    }
 
-        double getBalance(){
-            return balance;
-        }
+    double getBalance()
+    {
+        return balance;
+    }
 
+    void credit(double amount)
+    {
+        balance += amount;
+    }
+
+    void debit(double amount)
+    {
+        balance -= amount;
+    }
 };
-
 
 int main()
 {
-    
+
     return 0;
 }
