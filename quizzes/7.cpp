263e076db182f17736d6fd6540c527c5b272a046
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-09 22:31:13 +0630

    .

---
+++ quizzes/7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct Channel {
+    int channel_no;
+    string name;
+    int day;
+    int hour;
+    int min;
+};
+ 
+
+int main()
+{
+    Channel channel1;
+    channel1.day = 2;
+    cout << channel1.day;
+    return 0;
+}
