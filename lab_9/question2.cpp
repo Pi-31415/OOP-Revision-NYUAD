8ac6ca29148aec081142ff9633d7a6a9b3cc3058
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-09 17:44:31 +0630

    .

--- lab_9/question2.cpp
+++ lab_9/question2.cpp
@@ -5,19 +5,20 @@ using namespace std;
 class Time
 {
 private:
-    
+    int hour, minute, second;
 
 public:
     //Constructor
-    
-
-    
-
+    Time()
+    {
+        hour = 0;
+        minute = 0;
+        second = 0;
+    }
 };
 
 int main()
 {
-
 
     return 0;
 }
