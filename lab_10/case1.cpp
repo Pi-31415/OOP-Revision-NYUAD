11f73b120134588129caf3542d2e7cf29095fb90
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-16 17:44:02 +0630

    .

--- lab_10/case1.cpp
+++ lab_10/case1.cpp
@@ -9,25 +9,48 @@ private:
     int denominator;
 
 public:
-    Fraction(int n = 1, int d = 1){
-        numerator = n;
-        denominator = d;
+    Fraction(int n = 1, int d = 1)
+    {
+        int max = 0;
+        /*determine the larger value in fraction*/
+        if (n > d)
+        {
+            max = n;
+        }
+        else
+        {
+            max = d;
+        }
+
+        for (int i = 0; i <= max; i++)
+        {
+            if (n % i == 0 && d % i == 0)
+            {
+                numerator = n / i;
+                denominator = d / i;
+            }
+            else
+            {
+                numerator = n;
+                denominator = d;
+            }
+        }
     }
 
-    void printFraction(){
+    void printFraction()
+    {
         cout << numerator << "/" << denominator;
     }
 
-    int getNumerator(){
+    int getNumerator()
+    {
         return numerator;
     }
 
-    int getDenominator(){
+    int getDenominator()
+    {
         return denominator;
     }
-
-    
-
 };
 
 int main()
