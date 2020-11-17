2734f396f2884168582cafab576347ed0d746e02
Pi-31415 <paingthetko.pi@gmail.com>
2020-11-17 14:06:14 +0630

    .

--- quizzes/10.cpp
+++ quizzes/10.cpp
@@ -1,8 +1,34 @@
-#include <iostream>
+#include<iostream>
+#include<string>
 using namespace std;
 
+class Person{
+    private:
+    string name;
+    int birth_day;
+    int birth_year;
+    int birth_month;
+    static int num_people;
+
+    public:
+    int getNumPeople(){
+        return num_people;
+    }
+
+    Person(){
+        name = "";
+        birth_year = 1;
+        birth_day = 1;
+        birth_month = 1;
+        num_people++;
+    }
+};
+
+int Person::num_people = 0;
 
 int main(){
-    cout << "Hello World" << endl;
-    return 0;
+    Person* people = new Person[5];
+    Person Sarah = Person();
+    cout << Sarah.getNumPeople() << endl;
+    delete [] people;
 }
